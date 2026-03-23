6be84271d250639a21bb05af738b527c876b9da6
yuvrajsingh0125 <yuvismail2802@gmail.com>
2026-03-23 22:32:06 +0530

    feat: add /quiz/home route and wire GameHub as root index page

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter, RouterProvider, Route, Routes } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
-import QuizPlay from "../games/quiz/QuizPlay.jsx";
+import GameHub from "../games/GameHub.jsx";
 import QuizHomePage from "../games/quiz/QuizHomePage.jsx";
+import QuizPlay from "../games/quiz/QuizPlay.jsx";
 import QuizResult from "../games/quiz/QuizResult.jsx";
 
 export const router = createBrowserRouter([
@@ -9,9 +10,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <QuizHomePage /> },
+      { index: true, element: <GameHub /> },
+      { path: "quiz/home", element: <QuizHomePage /> },
       { path: "quiz/play", element: <QuizPlay /> },
       { path: "quiz/result", element: <QuizResult /> },
     ],
-    },
+  },
 ]);
